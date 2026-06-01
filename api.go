eddc85dab25630724b6bfdf55482328a533f68d1
Sahal Sabith <sahalsabith111@gmail.com>
2026-06-01 18:20:44 +0530

    completed till jwt auth

--- api.go
+++ api.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
 	"github.com/gorilla/mux"
+	jwt "github.com/golang-jwt/jwt/v5"
 )
 
 func WriteJSON( w http.ResponseWriter, status int, v any) error {
@@ -44,7 +44,7 @@ func NewAPIServer(listenAddr string, store Storage) *APIServer {
 func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
-	router.Handle("/account",  makeHTTPHandleFunc(s.handleAccount))
+	router.Handle("/account",  withJWTAuth(makeHTTPHandleFunc(s.handleAccount)))
 	router.Handle("/account/{id}",  makeHTTPHandleFunc(s.handleGetById))
 
 	log.Println("Json Api server running on port ",s.listenAddr)
@@ -92,6 +92,14 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 		return err
 	}
 
+	tokenString,err := CreateJWT(account)
+
+	if err != nil{
+		return err
+	}
+
+	fmt.Println("JWT token :",tokenString)
+
 	return WriteJSON(w, http.StatusOK, account)
 }
 
@@ -101,4 +109,46 @@ func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	return nil
+}
+
+func CreateJWT(account *Account) (string, error){
+	claims := &jwt.MapClaims{
+		"expiresAt": 15000,
+		"accountNumber":account.Number,
+	}
+
+	secret := "s4h41s4b1th"
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(secret))
+}
+
+func withJWTAuth(handlerFunc http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("calling jwt auth middleware....")
+
+		tokenStirng := r.Header.Get("x-jwt-token")
+
+		_, err := validateJWT(tokenStirng)
+
+		if err != nil {
+			WriteJSON(w, http.StatusForbidden, ApiError{Error: "invalid token"})
+			return 
+		}
+
+
+
+		handlerFunc(w,r)
+	}
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	secret := "s4h41s4b1th"
+
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _,ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil,fmt.Errorf("Unexpected signing method : %v",token.Header["alg"])
+		}
+		return []byte(secret),nil
+	})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/SahalSabith/Go-ecom
 go 1.26.3
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/lib/pq v1.12.3 // indirect
 )
